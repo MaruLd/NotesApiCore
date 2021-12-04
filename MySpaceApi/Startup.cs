169c1784749335e0811ebd21852e615b00b7c056
Maru <accnumberthree@gmail.com>
2021-12-04 19:13:27 +0700

    make it better

--- MySpaceApi/Startup.cs
+++ MySpaceApi/Startup.cs
@@ -44,12 +44,12 @@ namespace MySpaceApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MySpaceApi v1"));
-            }
+            
 
             app.UseHttpsRedirection();
 
